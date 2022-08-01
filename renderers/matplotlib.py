b4571d6291b937cbe67f892c9fdb1d0814901530
Manuel Wagner <m.wagner@aesir-interactive.com>
2022-08-01 17:27:52 +0200

    improved matplotlib preview

--- renderers/matplotlib.py
+++ renderers/matplotlib.py
@@ -15,17 +15,21 @@ def imshow(image, title=None):
 
 
 def predict_datapoint(datapoint, model):
-    fig, subplots = plt.subplots(3, 1, sharex=True, sharey=True, dpi=600)
-    for plot, name in zip(subplots, ("content", "style", "prediction")):
+    fig, subplots = plt.subplots(2, 2, sharex=True, sharey=True, dpi=600)
+
+    for plot, name in zip(subplots.flatten(), ("content", "style", "prediction", "prediction_remapped")):
         plot.title.set_text(name)
-    content_plot, style_plot, transfer_plot = subplots
+
+    content_plot, style_plot, prediction_plot, prediction_remapped_plot = subplots.flatten()
     content_plot.imshow(tf.squeeze(datapoint['content']))
     style_plot.imshow(tf.squeeze(datapoint['style']))
     prediction: tf.Tensor = tf.squeeze(model(datapoint))
+    prediction_remapped = prediction
     min, max = tf.reduce_min(prediction), tf.reduce_max(prediction)
     if min < 0 or max > 1:
         logging.warning(f"prediction has values that are not between 0 and 1: min: {min} max: {max}. Remapping")
-        prediction = (prediction.numpy() - min) / (max - min)
+        prediction_remapped = (prediction.numpy() - min) / (max - min)
 
-    transfer_plot.imshow(prediction)
+    prediction_plot.imshow(prediction)
+    prediction_remapped_plot.imshow(prediction_remapped)
     plt.show()
